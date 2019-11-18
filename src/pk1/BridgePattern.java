cbba2a6c81b2a6a9267093ebac787a508573f045
DELL <DELL@DELL-PC>
2019-11-18 09:48:49 +0530

    Added a git test line

--- src/pk1/BridgePattern.java
+++ src/pk1/BridgePattern.java
@@ -3,6 +3,7 @@ package pk1;
 public class BridgePattern {
 	public static void main(String[] args) 
     { 
+		System.out.println("Testing git");
         Vehicle vehicle1 = new Car(new Produce(), new Assemble()); 
         vehicle1.manufacture(); 
         Vehicle vehicle2 = new Bike(new Produce(), new Assemble()); 
