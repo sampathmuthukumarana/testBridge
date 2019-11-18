5afead8ec4b2b5801f60bfcf6c380e216ef275a5
Sampath <sampath4us80@gmail.com>
2019-11-18 11:39:36 +0530

    check cmd

--- src/pk1/BridgePattern.java
+++ src/pk1/BridgePattern.java
@@ -4,6 +4,7 @@ public class BridgePattern {
 	public static void main(String[] args) 
     { 
 		System.out.println("Testing git");
+		System.out.println("Testing cmd");
         Vehicle vehicle1 = new Car(new Produce(), new Assemble()); 
         vehicle1.manufacture(); 
         Vehicle vehicle2 = new Bike(new Produce(), new Assemble()); 
